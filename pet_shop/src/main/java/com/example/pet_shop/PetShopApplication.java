9448747ebef17998b7584e96cf7231fe449de0b8
Boil Radev <b.p.radev@gmail.com>
2023-04-13 05:29:44 -0900

    uploading project

---
+++ pet_shop/src/main/java/com/example/pet_shop/PetShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.pet_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetShopApplication.class, args);
+    }
+
+}
---
+++ pet_shop/src/test/java/com/example/pet_shop/PetShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pet_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
