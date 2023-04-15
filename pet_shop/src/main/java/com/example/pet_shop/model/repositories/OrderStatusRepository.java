fc9d01346188a1ee51c1877df042baf00cca856d
GeorgiKalachev <georgikalachev@yahoo.com>
2023-04-15 10:58:55 +0300

    adding controllers

--- pet_shop/src/main/java/com/example/pet_shop/model/repositories/OrderStatusRepository.java
+++ pet_shop/src/main/java/com/example/pet_shop/model/repositories/OrderStatusRepository.java
@@ -1,8 +1,9 @@
 package com.example.pet_shop.model.repositories;
 
+import com.example.pet_shop.model.entities.OrderStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrderStatusRepository extends JpaRepository<OrderRepository, Integer> {
+public interface OrderStatusRepository extends JpaRepository<OrderStatus, Integer> {
 }
