a0a2c9e473b6f0210c64d8e0b18b4f2e8b5809d1
GeorgiKalachev <georgikalachev@yahoo.com>
2023-04-15 10:56:55 +0300

    adding controllers

--- pet_shop/src/main/java/com/example/pet_shop/model/repositories/OrderRepository.java
+++ pet_shop/src/main/java/com/example/pet_shop/model/repositories/OrderRepository.java
@@ -1,6 +1,6 @@
 package com.example.pet_shop.model.repositories;
 
-import org.springframework.core.annotation.Order;
+import com.example.pet_shop.model.entities.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
