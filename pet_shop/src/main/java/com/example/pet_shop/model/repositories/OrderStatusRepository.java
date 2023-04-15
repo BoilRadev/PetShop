cf393b32eeaa17d10c412fa5df03735de225f552
Boil Radev <b.p.radev@gmail.com>
2023-04-14 23:07:05 -0900

    again records

--- pet_shop/src/main/java/com/example/pet_shop/model/repositories/OrderStatusRepository.java
+++ pet_shop/src/main/java/com/example/pet_shop/model/repositories/OrderStatusRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrderStatusRepository extends JpaRepository<OrderRepository, Integer> {
+public interface OrderStatusRepository extends JpaRepository<OrderStatus, Integer> {
 
 }
